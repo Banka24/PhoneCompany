4fa936f8718c371d0519b6b6de5f81a74294a8d1
Banka24 <Aq567489012@gmail.com>
2024-04-04 21:53:46 +0300

    .

--- PhoneCompany/View/Pages/AddAbonentPage.xaml.cs
+++ PhoneCompany/View/Pages/AddAbonentPage.xaml.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using PhoneCompany.ViewModel;
 
 namespace PhoneCompany.View.Pages
 {
     /// <summary>
     /// Логика взаимодействия для AddAbonentPage.xaml
     /// </summary>
-    public partial class AddAbonentPage : Page
+    public partial class AddAbonentPage
     {
         public AddAbonentPage()
         {
             InitializeComponent();
+            DataContext = new AddAbonentViewModel();
         }
     }
 }
---
+++ PhoneCompany/ViewModel/AddAbonentViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PhoneCompany.ViewModel;
+
+public class AddAbonentViewModel : INotifyPropertyChanged, INotifyDataErrorInfo
+{
+    public bool HasErrors => !string.IsNullOrWhiteSpace(NumberPhone) || NumberPhone!.Length >= 3 && NumberPhone![..3] is not "+7(" || NumberPhone!.Length <= 16;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+    private string _numberPhone = "+7(9__)___-__-__";
+
+    public string NumberPhone
+    {
+        get => _numberPhone;
+        set
+        {
+            _numberPhone = value;
+            ValidateProperty();
+            OnPropertyChanged();
+        }
+    }
+
+    public IEnumerable GetErrors(string propertyName)
+    {
+        if (propertyName is nameof(NumberPhone))
+        {
+            if (string.IsNullOrWhiteSpace(NumberPhone))
+            {
+                yield return "Номер обязателен";
+            }
+
+            else if (NumberPhone!.Length >= 3 && NumberPhone![..3] is not "+7(")
+            {
+                yield return "Введен номер неверного формата";
+            }
+
+            else if (NumberPhone!.Length < 16)
+            {
+                yield return "Длина должна быть 11 символов";
+            }
+        }
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void ValidateProperty([CallerMemberName] string propertyName = null)
+    {
+        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+    }
+}
